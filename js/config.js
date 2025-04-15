67c6cf14e6b720e44d6df26cea7943d6ecf519b9
Sabina Huseynzada <sebine.huseyn0@gmail.com>
2025-04-15 22:49:17 +0400

    deyisiklik config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const BASE_URL = {
-    GET: "https://papadata-2.onrender.com/",
-    POST: "https://papadata-2.onrender.com/",
-    PUT: "https://papadata-2.onrender.com/",
-    DELETE: "https://papadata-2.onrender.com/",
+    GET: "https://papadata-2.onrender.com",
+    POST: "https://papadata-2.onrender.com",
+    PUT: "https://papadata-2.onrender.com",
+    DELETE: "https://papadata-2.onrender.com",
 }
 
 export default BASE_URL
